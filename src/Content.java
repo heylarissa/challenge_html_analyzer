09c67cbc8143c18484f3291e3f6025f411393f9c
heylarissa <larissah.dandrade@gmail.com>
2023-03-27 03:10:27 -0300

    excluindo imports desnecessarios

--- src/Content.java
+++ src/Content.java
@@ -2,8 +2,6 @@ package src;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.ConnectException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Content {
